9e77848cc51cecb17d136a4b395ebf510577205e
Hanbo Wang <hanb.wang@gmail.com>
2023-07-04 13:49:21 -0700

    remove color

--- brass/board.py
+++ brass/board.py
@@ -1,15 +1,6 @@
 from __future__ import annotations
 from enum import Enum
 
-# class Color(Enum):
-#     BLUE = 1
-#     TEAL = 2
-#     YELLOW = 3
-#     BROWN = 4
-#     PURPLE = 5
-
-
-
 class GameBoard:
     def __init__(self) -> None:
         pass
